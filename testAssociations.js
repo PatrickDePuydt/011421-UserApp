c727d00a3d1c0a288156b6ed2a8c5c3b5144485f
PatrickDePuydt <prdepuydt@gmail.com>
2021-01-14 12:06:33 -0700

    main: fix error?

--- testAssociations.js
+++ testAssociations.js
@@ -17,15 +17,27 @@ const db = require('./models');
 // }).catch( error => console.log(`Error: ${error}`));
 
 
-db.user.findOne()
-.then( user => {
-  console.log(`🐕 Adding pet to ${user.name}`);
-  user.createPet({
-  // The createPet() method comes from the pet model
-    name: 'Spot',
-    species: 'dog',
-    description: 'loves live with their family'
-  }).then(createdPet => {
-    console.log(`${user.name} just adopted their pet ${createdPet.species} ${createdPet.name}`)
+// db.user.findOne()
+// .then( user => {
+//   console.log(`🐕 Adding pet to ${user.name}`);
+//   user.createPet({
+//   // The createPet() method comes from the pet model
+//     name: 'Spot',
+//     species: 'dog',
+//     description: 'loves live with their family'
+//   }).then(createdPet => {
+//     console.log(`${user.name} just adopted their pet ${createdPet.species} ${createdPet.name}`)
+//   });
+// });
+
+// Load Associated items into `getPet`
+db.user.findByPk(2)
+.then ( user => {
+  // Load pets for this user
+  user.getPets()
+  .then( pets => {
+    console.log(`🚀 pets:`, pets)
+  }).catch(error => {
+    console.log(`❌3: error`, error);
   });
 });
